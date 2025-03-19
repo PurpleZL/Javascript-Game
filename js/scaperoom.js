96a010dfe4791012cbeea621c2555a73b24a44ca
PurpleZL <vladutorza64@gmail.com>
2025-03-19 01:17:10 +0100

    Se han añadido funcionalidades: - Boton Reinicio - Se añadio el final de la troll Se arreglaron aspectos visuales

--- js/scaperoom.js
+++ js/scaperoom.js
@@ -7,7 +7,7 @@ function moverse1() {
   document.getElementById("principal").style.backgroundImage = 'url("../img/Escenarios/2 puertas.jpg")';
   document.getElementById("señal1").style.display = "none";
   document.getElementById("parrafo1").innerHTML = "-Seguro que ese diablillo sabe algo";
-  document.getElementById("parrafo2").innerHTML = "+Soy Añañiiin, si quieres recuperar a Enrique tendras que pasar mis pruebas, elige una puerta";
+  document.getElementById("parrafo2").innerHTML = "Añañin: Soy Añañiiin, si quieres recuperar a Enrique tendras que pasar mis pruebas, elige una puerta";
 
   document.getElementById("portalIzq").style.display = "block";
   document.getElementById("portalDer").style.display = "block";
@@ -21,12 +21,43 @@ function camino(direccion) {
     document.getElementById("principal").style.backgroundImage = 'url("../img/Escenarios/3puertas.jpg")';
     document.getElementById("señal1").style.display = "none";
     document.getElementById("parrafo1").innerHTML = "Pregunta 1";
-    document.getElementById("parrafo2").innerHTML = "+Si una troll fan de Taylor Swift se tira tremendo cuesco en las montañas y nadie está cerca para escucharlo, ¿hace ruido?";
+    document.getElementById("parrafo2").innerHTML = "Añañin: Si una troll fan de Taylor Swift se tira tremendo cuesco en las montañas y nadie está cerca para escucharlo, ¿hace ruido?";
 
     document.getElementById("portalIzq").style.display = "none";
     document.getElementById("portalDer").style.display = "none";
 
     // Mostrar el contenedor de respuestas
     document.getElementById("respuestasContainer").style.display = "flex";
+  }//else if direccion es derecha ...
+}
+
+function mostrarRespuesta1Izq(respuesta){
+  if (respuesta == "A") {
+    
+  }else{//Respuesta Incorrecta
+    document.getElementById("principal").style.backgroundImage = 'url("../img/Escenarios/montañasombria.jpg")';
+
+    document.getElementById("parrafo1").innerHTML = "Voz Misteriosa: La troll te mira indignada y con ojos de desquiciada, sabes que tu respuesta no le ha gustado.";
+    document.getElementById("parrafo2").innerHTML = "Voz Misteriosa: En tus ultimos segundos de vida ves como se dirige hacia ti corriendo, es el fin... Mueres ";
+
+    document.getElementById("troll").style.display = "block";
+    document.getElementById("censure1").style.display = "block";
+    document.getElementById("censure2").style.display = "block";
+    document.getElementById("censure3").style.display = "block";
+    document.getElementById("reiniciar").style.display = "block";
+
+    document.getElementById("diablillo").style.display = "none";
+    document.getElementById("respuestasContainer").style.display = "none";
   }
+}
+
+//Funcion divertida
+function pervertido(){
+  document.getElementById("parrafo1").innerHTML = "Troll: ¡¡PERVERTIDO!!";
+  document.getElementById("parrafo2").innerHTML = "Voz Misteriosa: Resulta que la troll tiene OF, te costara $4.99 suscribirte";
+}
+
+//Esta es la funcion que sirve para reiniciar el juego en caso de muerte o victoria
+function reiniciarJuego() {
+  location.reload(); // Recarga la página para reiniciar el juego
 }
